865a9d12868ac04a0efeae5214e21f6512c4e054
Oreoluwa <orezyjosh@gmail.com>
2023-05-02 12:49:37 +0100

    Made changes on the article page

---
+++ src/components/MySkills.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+const Skill = ({ SkillName, x, y }) => {
+  return (
+    <motion.div
+      className="flex items-center justify-center rounded-full font-semibold bg-dark text-light py-3 px-6 shadow-dark cursor-pointer absolute"
+      whileHover={{ scale: 1.05 }}
+      initial={{
+        x: 0,
+        y: 0,
+      }}
+      whileInView={{ x: x, y: y }}
+      transition={{ duration: 1.5 }}
+      viewport={{ once: true }}
+    >
+      {SkillName}
+    </motion.div>
+  );
+};
+const MySkills = () => {
+  return (
+    <>
+      <h2 className="font-bold text-8xl mt-64 w-full text-center">Skills</h2>
+      <div className="w-ful h-screen relative flex items-center justify-center rounded-full bg-circularLight">
+        <motion.div
+          className="flex items-center justify-center rounded-full font-semibold bg-dark text-light p-8 shadow-dark cursor-pointer"
+          whileHover={{ scale: 1.05 }}
+        >
+          web
+        </motion.div>
+
+        <Skill SkillName="HTML" x="-25vw" y="2vw" />
+        <Skill SkillName="CSS" x="-5vw" y="-10vw" />
+        <Skill SkillName="Javascript" x="20vw" y="6vw" />
+        <Skill SkillName="ReactJS" x="0vw" y="12vw" />
+        <Skill SkillName="VueJs" x="-20vw" y="-15vw" />
+        <Skill SkillName="NextJs" x="15vw" y="-12vw" />
+        <Skill SkillName="Web Design" x="32vw" y="-5vw" />
+        <Skill SkillName="Bootstrap CSS" x="0vw" y="-20vw" />
+        <Skill SkillName="Firebase" x="-25vw" y="18vw" />
+        <Skill SkillName="Materialize CSS" x="18vw" y="18vw" />
+      </div>
+    </>
+  );
+};
+
+export default MySkills;
---
+++ src/pages/article.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Head from 'next/head'
+import Layout from '@/components/Layout'
+import AnimatedTexts from '@/components/AnimatedTexts'
+const article = () => {
+  return (
+    <>
+      <Head>
+        <title>  {"Ore's"} | About Page </title>
+        <meta name="description" content="article page" />
+      </Head>
+      <main>
+        <Layout className='pt-16'>
+            <AnimatedTexts text="Words Can Change The World!" className='mb-16' />
+        </Layout>
+      </main>
+    
+    </>
+  )
+}
+
+export default article
